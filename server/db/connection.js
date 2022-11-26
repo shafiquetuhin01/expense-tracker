50877eea66b3724bb00b0f9cd3d584f916f3e673
unknown <sfqtuhin04@gmail.com>
2022-11-26 08:29:04 +0600

    expense tracker server updated

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,13 +1,11 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const conn = mongoose
-  .connect(process.env.ATLAS_URI)
-  .then((db) => {
-    console.log("Database connected");
-    return db;
-  })
-  .catch((err) => {
-    console.log("Connection error");
-  });
+const conn = mongoose.connect(process.env.ATLAS_URI)
+        .then(db => {
+            console.log("Database Connected");
+            return db;
+        }).catch(err => {
+            console.log("Connection Error");
+        })
 
 module.exports = conn;
